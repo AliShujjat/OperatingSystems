c10f318926c98f9d28602118409f6f600ede3814
Ali Shujjat <48991374+AliShujjat@users.noreply.github.com>
2020-12-19 13:08:52 +0500

    Add files via upload

---
+++ malloc/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "Lec2_st03856_A3_malloc.h"
+
+int main(void)
+{
+	// int checkInit = my_init();
+	// if (checkInit == 0)
+	// {
+	// 	exit(0);
+	// }
+
+	// void* ptr = my_malloc(2000);
+	// void* ptr1 = my_malloc(50000);
+	// void* ptr2= my_malloc(200);
+	// void* ptr3 = my_malloc(500);
+	// void* ptr4 = my_malloc(3000);
+	// void* ptr5 = my_calloc(10,600);
+
+	// my_free(ptr);
+	// my_free(ptr1);
+
+	// my_showfreelist();
+	// my_coalesce();
+	// my_showfreelist();
+
+	// my_free(ptr4);
+	// my_free(ptr5);
+
+	// my_showfreelist();
+	// my_coalesce();
+	// my_showfreelist();
+	
+	// my_uninit();
+	// return 0;
+}
